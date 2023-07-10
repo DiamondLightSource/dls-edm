0f1e70caabf52dfb7a628dd930c2116af1166397
Ollie Copping <oliver.copping@diamond.ac.uk>
2023-07-10 10:30:22 +0100

    Added noqa F401 too all imports; Fixed docstring warning; Formatting

--- dls_edm/__init__.py
+++ dls_edm/__init__.py
@@ -2,30 +2,30 @@
 # dls.edm.<function>
 # defining __all__ exports only useful functions to the outside world
 
-"""dls.edm - Module containing a python representation of an edm object"""
+"""dls.edm - Module containing a python representation of an edm object."""
+from . import common  # noqa: F401
+from .edmObject import EdmObject, quoteListString, quoteString  # noqa: F401
+from .edmTable import EdmTable  # noqa: F401
+from .flip_horizontal import Flip_horizontal  # noqa: F401
+from .generic import Generic  # noqa: F401
+from .guibuilder import ERROR, SILENT, WARN, GuiBuilder  # noqa: F401
+from .resize import Resize  # noqa: F401
+from .substitute_embed import Substitute_embed  # noqa: F401
+from .summary import Summary  # noqa: F401
+from .titlebar import Titlebar  # noqa: F401
+from .vacuum import Vacuum  # noqa: F401
 
-__all__=[]
-		
-from .edmObject import EdmObject,quoteString,quoteListString
-__all__.extend(["EdmObject","quoteString","quoteListString"])
-from .edmTable import EdmTable
+__all__ = []
+
+__all__.extend(["EdmObject", "quoteString", "quoteListString"])
 __all__.extend(["EdmTable"])
-from .titlebar import Titlebar
 __all__.extend(["Titlebar"])
-from .resize import Resize
 __all__.extend(["Resize"])
-from .generic import Generic
 __all__.extend(["Generic"])
-from .summary import Summary
 __all__.extend(["Summary"])
-from .vacuum import Vacuum
 __all__.extend(["Vacuum"])
-from .substitute_embed import Substitute_embed
 __all__.extend(["Substitute_embed"])
-from .flip_horizontal import Flip_horizontal
 __all__.extend(["Flip_horizontal"])
-from .guibuilder import GuiBuilder, SILENT, WARN, ERROR
-__all__.extend(['GuiBuilder', 'SILENT', 'WARN', 'ERROR'])
-from . import common
+__all__.extend(["GuiBuilder", "SILENT", "WARN", "ERROR"])
 __all__.extend(["common"])
 __all__.sort()
